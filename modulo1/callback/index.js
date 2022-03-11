946bfc244426213c479da59139e25ebfe328f2d8
EduardoOrnelaVaz <eduardo.ornela@gmail.com>
2022-03-10 22:24:17 -0300

    Exercício de callback

---
+++ modulo1/callback/index.js
@@ -0,0 +1,42 @@
+//Exercícios de interpretação de código
+//1- Leia o código abaixo
+
+//const usuarios = [
+//  { nome: "Amanda Rangel", apelido: "Mandi" },
+//  { nome: "Laís Petra", apelido: "Laura" },
+//  { nome: "Letícia Chijo", apelido: "Chijo" }
+//]
+
+//const novoArrayA = usuarios.map((item, index, array) => {
+//   console.log(item, index, array)
+//})
+// Resposta:Aparece os nomes e apelidos.
+
+//2-
+
+//const usuarios = [
+//    { nome: "Amanda Rangel", apelido: "Mandi" },
+//    { nome: "Laís Petra", apelido: "Laura" },
+//    { nome: "Letícia Chijo", apelido: "Chijo" },
+//  ]
+  
+//  const novoArrayB = usuarios.map((item, index, array) => {
+//     return item.nome
+//  })
+  
+//  console.log(novoArrayB)
+//Resposta: Aparece as arrays
+  
+//3-
+
+//const usuarios = [
+//   { nome: "Amanda Rangel", apelido: "Mandi" },
+//  { nome: "Laís Petra", apelido: "Laura" },
+//  { nome: "Letícia Chijo", apelido: "Chijo" },
+// ]
+ 
+// const novoArrayC = usuarios.filter((item, index, array) => {
+//    return item.apelido !== "Chijo"
+// })
+ //console.log(novoArrayC)
+ // Resporta: Aparece array 1 e 2.
