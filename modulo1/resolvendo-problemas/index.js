c811a833d2ecbffee92282ce40c36c60719d3964
EduardoOrnelaVaz <eduardo.ornela@gmail.com>
2022-03-15 20:26:15 -0300

    Exercícios Resolvendo-problemas

---
+++ modulo1/resolvendo-problemas/index.js
@@ -0,0 +1,29 @@
+// Exercícios de fixação do conhecimento
+
+//Faça um resumo sobre quais são os 7 passos fundamentais para a resolução de problemas e como eles podem nos ajudar a solucionar nossos desafios de programação.
+
+//1º Atenção no enunciado
+// Duas perguntas deverão der feitas, para melhor compreenção do enunciado.
+// O que o código deve fazer?
+//Quais informações são unicas?
+
+//2º Retirar informações relevante
+//Para essa etapa precisam ser feitas duas perguntas.
+//Quais são as palavras chaves?
+//Informações relevantes para o código?
+
+//3º Pensar nas Entradas e Saídas.
+//Pensar nos dados recebidos, e qual possível resposta dentro código.
+//Criar uma função com as entradas e saídas pensadas.
+
+//4º Fazer um esquema visual.
+// Fazer um fluxograma com as possiveis entradas e saídas.
+
+//5º Criar um código feio.
+// Colocar no papel todas soluções pensadas, escolher as melhores e testar cada uma.
+
+//6º Testar força-bruta.
+//testar a melhor solução, escrever o código em português e transcrever para linguagem de programação desejada.
+
+//7º Refatorar.
+//Escolher nomes apropriados para variáveis e funções, separar as funções e começar pelas etapas principais.
